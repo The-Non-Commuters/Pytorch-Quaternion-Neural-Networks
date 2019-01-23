a8394cb63efaa46005170349e75ae93f0baf26f1
Riccardo Vecchi <r.vecchi92@gmail.com>
2019-01-23 14:41:11 +0100

    clean utils

--- utils/psnr_ssim.py
+++ utils/psnr_ssim.py
@@ -6,24 +6,26 @@
 # October 2018
 ##########################################################
 
-import numpy 
-import math
-from scipy           import misc
-import sys
 from skimage.measure import compare_ssim as ssim
+import imageio
+import numpy
+import math
+import sys
 
-original = misc.imread(sys.argv[1])
-contrast = misc.imread(sys.argv[2])
 
 def psnr(img1, img2):
-    mse = numpy.mean( (img1 - img2) ** 2 )
+    mse = numpy.mean((img1 - img2) ** 2)
     if mse == 0:
         return 100
-    PIXEL_MAX = 255.0
-    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+    pixel_max = 255.0
+    return 20 * math.log10(pixel_max / math.sqrt(mse))
 
-p = psnr(original,contrast)
-s = ssim(original, contrast, multichannel=True)
 
-print("PSNR : "+str(p))
-print("SSIM : "+str(s))
+original = imageio.imread(sys.argv[1])
+contrast = imageio.imread(sys.argv[2])
+
+p = psnr(original, contrast)  # Peak signal-to-noise ratio
+s = ssim(original, contrast, multichannel=True)  # Structural similarity
+
+print("PSNR : " + str(p))
+print("SSIM : " + str(s))
